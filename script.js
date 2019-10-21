6a14256b76ae2ff3d3e7302ac353a61b486973e3
jamie <jamie2119@hotmail.com>
2019-10-20 20:22:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const quotes = [
+    {
+        name: 'author number 1',
+        quote: 'quote number 1'
+    },
+
+    {
+        name: 'author number 2',
+        quote: 'quote number 2'
+    },
+
+    {
+        name: 'author number 3',
+        quote: 'quote number 3'
+    },
+
+    {
+        name: 'author number 4',
+        quote: 'quote number 4'
+    },
+]
+
+const quoteBtn = document.querySelector('#quoteBtn');
+const quoteAuthor = document.querySelector('#quoteAuthor');
+const quote = document.querySelector('#quote');
+
+quoteBtn.addEventListener('click', displayQuote);
+
+function displayQuote () {
+    let number = Math.floor(Math.random()*quotes.length);
+    quoteAuthor.innerHTML = quotes[number].name;
+    quote.innerHTML = quotes[number].quote;
+}
+
+
