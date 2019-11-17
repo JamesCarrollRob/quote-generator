605cbe80238416315b56c9ccfe11c1a5e8c81083
jamie <jamie2119@hotmail.com>
2019-11-17 14:08:30 -0500

    added jquery, animation, changed background colors

--- script.js
+++ script.js
@@ -1,35 +1,72 @@
+
+
 const quotes = [
     {
-        name: 'author number 1',
-        quote: 'quote number 1'
+        name: 'Marcus Aurelius',
+        quote: 'You have power over your mind - not outside events. Realize this, and you will find strength.'
     },
 
     {
-        name: 'author number 2',
-        quote: 'quote number 2'
+        name: 'Plato',
+        quote: 'If you do not take an interest in the affairs of your government, then you are doomed to live under the rule of fools.'
     },
 
     {
-        name: 'author number 3',
-        quote: 'quote number 3'
+        name: 'Ho Chi Minh',
+        quote: 'Today we should make poems including iron and steel And the poet should know how to lead an attack.'
     },
 
     {
-        name: 'author number 4',
-        quote: 'quote number 4'
+        name: 'Friedrich Nietzsche',
+        quote: 'There will always be rocks in the road ahead of us. They will be stumbling blocks or stepping stones; it all depends on how you use them.'
     },
 ]
+
+
 
 const quoteBtn = document.querySelector('#quoteBtn');
 const quoteAuthor = document.querySelector('#quoteAuthor');
 const quote = document.querySelector('#quote');
 
-quoteBtn.addEventListener('click', displayQuote);
+quoteBtn.addEventListener('click', quoteAnimation);
 
-function displayQuote () {
-    let number = Math.floor(Math.random()*quotes.length);
-    quoteAuthor.innerHTML = quotes[number].name;
-    quote.innerHTML = quotes[number].quote;
+function quoteAnimation () {
+    $('.container').fadeOut(2000).fadeIn(4000);
+    displayQuote();
+        
+    // $('#quote').fadeOut(3000);
+    // $('#quoteAuthor').fadeOut(3000);
+    //    setTimeout(displayQuote(),
+    //     5000);     
 }
 
 
+function displayQuote () {
+    let number = Math.floor(Math.random()*quotes.length);
+    setTimeout( function (){
+    quoteAuthor.innerHTML = quotes[number].name;
+    quote.innerHTML = quotes[number].quote; 
+    },2000);
+}
+
+
+
+
+
+
+// $( document ).ready(function() {
+//     $('#quoteBtn').click(function(){
+//     $('h2').fadeIn(3000);
+ 
+//     });
+// });
+
+
+// $( document ).ready(function quoteAnimation () {
+    
+//     $('#quoteBtn').click(function(){
+//     $('#quote').fadeOut(3000).displayQuote().fadeIn(3000);
+//     $('#quoteAuthor').fadeOut(3000).displayQuote().fadeIn(3000);
+ 
+//     });
+// });
